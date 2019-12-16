d13df319316c0b69dfa546e8970aa3e857070bcc
Romain Marquois <rmarquois@ubidreams.com>
2019-12-16 15:22:36 +0100

    Support for cropping options on iOS
    - options are `width`, `height` and `thumbnail`
    - promise returns an object with `image` and `thumbnail` (files paths)

--- ios/RNDocumentScanner.m
+++ ios/RNDocumentScanner.m
@@ -18,17 +18,17 @@ RCT_REMAP_METHOD(detectEdges,
     // get image from path
     UIImage *image = [UIImage imageWithContentsOfFile:imagePath];
     image = [image fixOrientation];
-
+    
     // get layout
     CGFloat containerX = [[layout valueForKey:@"x"] doubleValue];
     CGFloat containerY = [[layout valueForKey:@"y"] doubleValue];
     CGFloat containerWidth = [[layout valueForKey:@"width"] doubleValue];
     CGFloat containerHeight = [[layout valueForKey:@"height"] doubleValue];
-
+    
     // configure image
     self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake(containerX, containerY, containerWidth, containerHeight)];
     self.imageView.image = image;
-
+    
     // go opencv !
     CropRect cropRect = [self.imageView detectEdges];
 
@@ -51,7 +51,7 @@ RCT_REMAP_METHOD(detectEdges,
           @"y": [NSNumber numberWithDouble:cropRect.bottomLeft.y]
       }
     ];
-
+    
     // resolve promise
     resolve(points);
 }
@@ -62,50 +62,83 @@ RCT_REMAP_METHOD(crop,
                  resolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject)
 {
+    NSMutableDictionary *result = [[NSMutableDictionary alloc] init];
+    
     // get points
     NSDictionary *topLeftPoint = [points objectAtIndex:0];
     NSDictionary *topRightPoint = [points objectAtIndex:1];
     NSDictionary *bottomRightPoint = [points objectAtIndex:2];
     NSDictionary *bottomLeftPoint = [points objectAtIndex:3];
-
+ 
     // get options
-    float width = [[options valueForKey:@"width"] floatValue];
-    float height = [[options valueForKey:@"height"] floatValue];
-
+    double width = [[options valueForKey:@"width"] doubleValue];
+    double height = [[options valueForKey:@"height"] doubleValue];
+    BOOL thumbnail = [[options valueForKey:@"thumbnail"] boolValue];
+    
     // go opencv !
     CropRect cropRect;
     cropRect.topLeft = CGPointMake([[topLeftPoint objectForKey:@"x"] doubleValue], [[topLeftPoint objectForKey:@"y"] doubleValue]);
     cropRect.topRight = CGPointMake([[topRightPoint objectForKey:@"x"] doubleValue], [[topRightPoint objectForKey:@"y"] doubleValue]);
     cropRect.bottomRight = CGPointMake([[bottomRightPoint objectForKey:@"x"] doubleValue], [[bottomRightPoint objectForKey:@"y"] doubleValue]);
     cropRect.bottomLeft = CGPointMake([[bottomLeftPoint objectForKey:@"x"] doubleValue], [[bottomLeftPoint objectForKey:@"y"] doubleValue]);
-
+    
     UIImage *croppedImage = [self.imageView crop:cropRect andApplyBW:false];
-
-    // resize image ?
+    
+    // resize cropped image ?
     if (width > 0 && height > 0) {
-        float ratioX = width / croppedImage.size.width;
-        float ratioY = height / croppedImage.size.height;
-        float ratio = MIN(ratioX, ratioY);
-
-        float finalWidth = croppedImage.size.width * ratio;
-        float finalHeight = croppedImage.size.height * ratio;
-
-        UIGraphicsBeginImageContext(CGSizeMake(finalWidth, finalHeight));
-        [croppedImage drawInRect:CGRectMake(0, 0, finalWidth, finalHeight)];
-
-        croppedImage = UIGraphicsGetImageFromCurrentImageContext();
-
-        UIGraphicsEndImageContext();
+        croppedImage = [self resizeImage:croppedImage toWidth:width andHeight:height];
     }
-
+    
     // save image to cache directory
-    NSString *cacheDirPath = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) firstObject];
-    NSString *croppedImageFilePath = [cacheDirPath stringByAppendingPathComponent:[[NSProcessInfo processInfo] globallyUniqueString]];
-
-    [UIImageJPEGRepresentation(croppedImage, 1) writeToFile:croppedImageFilePath atomically:YES];
-
+    NSString *croppedImageFilePath = [self saveUIImageToCacheDirectory:croppedImage];
+    
+    // add image file path to result
+    [result setValue:[NSString stringWithFormat:@"file://%@", croppedImageFilePath] forKey:@"image"];
+    
+    // create thumbnail ?
+    if (thumbnail) {
+        // resize original image to create a thumbnail
+        UIImage *thumbnailImage = [self resizeImage:croppedImage toWidth:width andHeight:height];
+        
+        // save thumbnail to cache directory
+        NSString *thumbnailFilePath = [self saveUIImageToCacheDirectory:thumbnailImage];
+        
+        // add thumbnail file path to result
+        [result setValue:[NSString stringWithFormat:@"file://%@", thumbnailFilePath] forKey:@"thumbnail"];
+    }
+    
     // resolve promise
-    resolve(croppedImageFilePath);
+    resolve(result);
+}
+
+- (UIImage *)resizeImage:(UIImage *)image toWidth:(double)width andHeight:(double)height
+{
+    CGSize size = CGSizeMake(width, height);
+    CGFloat scale = MAX(size.width / image.size.width, size.height / image.size.height);
+    CGFloat scaledWidth = image.size.width * scale;
+    CGFloat scaledHeight = image.size.height * scale;
+    CGRect imageRect = CGRectMake((size.width - scaledWidth)/2.0f,
+                                  (size.height - scaledHeight)/2.0f,
+                                  scaledWidth,
+                                  scaledHeight);
+    
+    UIGraphicsBeginImageContextWithOptions(size, NO, 0);
+    [image drawInRect:imageRect];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
+- (NSString *)saveUIImageToCacheDirectory:(UIImage *)image
+{
+    NSString *fileName = [NSString stringWithFormat:@"%@.jpg", [[NSProcessInfo processInfo] globallyUniqueString]];
+    NSString *cacheDirPath = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) firstObject];
+    NSString *imageFilePath = [cacheDirPath stringByAppendingPathComponent:fileName];
+    
+    [UIImageJPEGRepresentation(image, 1) writeToFile:imageFilePath atomically:YES];
+    
+    return imageFilePath;
 }
 
 @end
